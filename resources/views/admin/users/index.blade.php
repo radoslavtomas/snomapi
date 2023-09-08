5427222e6f4f46b0c37cf517ea7ab2f26bc37cd3
Radoslav Tomas <radoslav.tomas@gmail.com>
2023-09-08 16:41:19 +0100

    configured base for admin section

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
+
+    <div class="container mx-auto">
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 text-white">
+            @foreach($users as $user)
+                <h1>{{ $user->email }}</h1>
+            @endforeach
+        </div>
+
+        <div class="">
+            {{ $users->links() }}
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::prefix('admin')->group(function () {
+        Route::resource('users', UserController::class);
+    });
 });
